ae8c88574c27ba2b74fc342ee37ba534f9b8983f
sahidulislam <sahidul.islam@rootnext.com>
2016-12-06 19:55:05 +0600

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+
+var db = mongojs('mongodb://root:123456@ds025459.mlab.com:25459/meantodos', ['todos']);
+
+// GET TODOS
+router.get('/todos', function(req, res, next){
+	db.todos.find(function(err, todos){
+		if(err){
+			res.send(err);
+		}else{
+			res.json(todos);
+		}
+	});
+});
+
+
+// GET Single todos
+router.get('/todo/:id', function(req, res, next){
+	db.todos.findOne({
+		_id: mongojs.ObjectId(req.params.id)
+	},function(err, todo){
+		if(err){
+			res.send(err);
+		}else{
+			res.json(todo);
+		}
+	});
+});
+
+
+// save todos
+router.post('/todo/',function(req, res, next){
+	var todo = req.body;
+	if(!todo.text || !(todo.isCompleted + '')){
+		res.status(400);
+		res.json({
+			"error": "Invalid Data."
+		})
+	}else{
+		db.save(todo, function(err, result){
+			if(err){
+				res.send(err);
+			}else{
+				res.json(result);
+			}
+
+		})
+	}
+});
+
+// update todos
+router.put('/todo/:id',function(req, res, next){
+	var todo = req.body;
+	var updatedObject = {};
+
+	if(todo.isCompleted){
+		updatedObject.isCompleted = todo.isCompleted;
+	}
+	if(todo.text){
+		updatedObject.text = todo.text;
+	}
+	if(!updatedObject){
+		res.status(400);
+		res.json({
+			"error": "Invalid Data."
+		})
+	}else{
+		db.todos.update({
+			_id: mongojs.ObjectId(req.params.id)
+		}, updatedObject, {}, function(err, result){
+			if(err){
+				res.send(err);
+			}else{
+				res.json(result);
+			}
+
+		})
+	}
+
+});
+
+// delete todos
+router.put('/todo/:id',function(req, res, next){
+	db.todos.remove({
+		_id: mongojs.ObjectId(req.params.id)
+	}, '', function(err, result){
+		if(err){
+			res.send(err);
+		}else{
+			res.json(result);
+		}
+	})
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+
+var app = express();
+
+// View Engine
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname,'client')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api/v1/', todos);
+
+
+app.listen(3000, function(){
+	console.log('Server started on port 3000...');
+});
